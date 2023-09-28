224eb0bcef8ffe0aa6217b2dc7da90824f432355
Akramerabti <akramerabti@gmail.com>
2023-09-28 17:04:52 -0400

    s

--- public/js/videoCall.js
+++ public/js/videoCall.js
@@ -1,10 +1,17 @@
-document.getElementById('voiceChat').addEventListener('click', function() {
-    navigator.mediaDevices.getUserMedia({ video: true, audio: true }).then(function(stream) {
-    var video = document.createElement('video');
-    document.body.appendChild(video);
-    video.srcObject = stream;
-    video.play();
-    }).catch(function(err) {
-    console.log(err);
+const voiceChatButton = document.getElementById('voiceChat');
+const videoContainer = document.getElementById('video-container');
+
+voiceChatButton.addEventListener('click', () => {
+  navigator.mediaDevices.getUserMedia({ video: true, audio: true })
+    .then(function (stream) {
+      // Hide the button and display the video container
+      voiceChatButton.style.display = 'none';
+      videoContainer.style.display = 'block';
+
+      const video = document.getElementById('video');
+      video.srcObject = stream;
+    })
+    .catch(function (err) {
+      console.error(err);
     });
-    });
+});
